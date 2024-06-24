da7e3b0934adb9515c2bc337a3fad6ac3cda9b85
Aakash Agarwal <aakash@instrik.com>
2024-06-24 13:30:36 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'story_brain.dart';
 
 void main() => runApp(Destini());
-// trial comment 17
+// trial comment 18
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(

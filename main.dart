acaf79b08b5611feb1ed8e6a38cd9e0102c2954b
Aakash Agarwal <agarwal7409@gmail.com>
2026-02-25 08:30:35 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'story_brain.dart';
 
 void main() => runApp(Destini(
 
-// trial comment jklnm njmk
+// trial comment jklnm njmkbn
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -113,6 +113,7 @@ class _StoryPageState extends State<StoryPage> {
                         ),
 
                       )
+
 
 
 

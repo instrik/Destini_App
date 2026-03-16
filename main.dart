00bd665bf463d02c09d52bc93a0663be4f1dc7d4
Aakash Agarwal <agarwal7409@gmail.com>
2026-03-16 06:05:30 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'story_brain.dart';
 
 void main() => runApp(Destini(
 
-// trial  knj jk
+// trial  knj jkkljkn
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {

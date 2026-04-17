57a2bcf14810ceb709cb9d81d3e33bd9286a96bd
Aakash Agarwal <agarwal7409@gmail.com>
2026-04-17 14:23:58 +0530

    Refactor main.dart to improve code structure

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'story_brain.dart';
 
 void main() => runApp(Destini(
 
-// trial comment jlmk
+// trial comment jlmkmpom
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {

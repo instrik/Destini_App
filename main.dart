ce19d5b73c924c660412943ca64b88300d4df313
Aakash Agarwal <agarwal7409@gmail.com>
2026-04-21 23:58:30 +0530

    Refactor main.dart to remove unnecessary comments

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'story_brain.dart';
 
 void main() => runApp(Destini(
 
-// trial comment fgfgf
+// trial comment fgfgfjgl/
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {

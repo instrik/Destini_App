a7d3f0819e10a2c1ac1f7e40dda0148b7e2b15fa
Aakash Agarwal <agarwal7409@gmail.com>
2026-05-03 23:52:46 +0530

    Refactor main.dart to remove commented code

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'story_brain.dart';
 
 void main() => runApp(Destini(
 
-// trial comment  kj 'jh
+// trial comment  kj 'jhjnj;k
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {

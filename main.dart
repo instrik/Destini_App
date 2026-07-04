fd15d3ef842e99c93281db264d129ba17c6a64ed
Aakash Agarwal <agarwal7409@gmail.com>
2026-07-04 18:28:46 +0530

    Reformat main.dart and remove comments

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'story_brain.dart';
 
-// trial comment jk
+// trial comment kjjkn
 
 void main() => runApp(Destini(
 

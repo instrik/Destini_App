236713dd554fe8a02807b91846a42681b47773ef
Aakash Agarwal <agarwal7409@gmail.com>
2026-07-04 18:30:35 +0530

    Reformat main.dart and restore imports

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'story_brain.dart';
 
-// trial comment kjjknjkbjjkjjkjk
+// trial comment kjjknjkbjjkjjkjkjkjj
 
 void main() => runApp(Destini(
 
